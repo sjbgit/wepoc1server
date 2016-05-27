1a0296b8a2e1f026d69511c2ef1e3ff5361967f0
Seth Bunke <seth.bunke@bl-1614.blackline.corp>
2016-05-27 15:39:04 -0700

    update app.js to serve static files

--- app.js
+++ app.js
@@ -37,8 +37,9 @@ var quizRouter = require('./routes/quizRouter');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.use(express.static(path.join(__dirname, 'views')));
+//app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'jade');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
